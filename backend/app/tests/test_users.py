e4b5d93d89755e572130c35a76e9294179d606c4
Curious-Keeper <brian@scalingsuccess.io>
2025-03-08 13:13:19 -0500

    Reorganize repository structure while preserving submodules

--- backend/app/tests/test_users.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-from fairgig.main import app
-
-client = TestClient(app)
-
-def test_get_users():
-    response = client.get("/users/")
-    assert response.status_code == 200
-    assert isinstance(response.json(), list)
